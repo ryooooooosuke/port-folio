9e785a01485e664d277b99d836c94004cc056ce6
ryooooooosuke <ryosuke.git@gmail.com>
2022-03-20 10:10:07 +0900

    Change to wordpress

---
+++ footer.php
@@ -0,0 +1,21 @@
+    <footer>
+        <div class="container">
+            <div class="social">
+                <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i>  </a>
+                <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i>  </a>
+                <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i>  </a>
+            </div>
+            <p>Copyright © 2016 Stephans, All rights Reserved. <br/> Created by Tavillathemes</p>
+        </div>
+    </footer>
+    <!-- Javascripts -->
+    <script src="<?php bloginfo('template_url') ;?>/js/jquery-2.1.4.min.js"></script>
+    <!-- jQuery library -->
+    <script src="<?php bloginfo('template_url') ;?>/js/bootstrap.min.js"></script>
+    <script src="<?php bloginfo('template_url') ;?>/js/wow.min.js"></script>
+    <script src="<?php bloginfo('template_url') ;?>/js/isotope.pkgd.min.js"></script>
+    <script src="<?php bloginfo('template_url') ;?>/js/typed.js"></script>
+    <script src="<?php bloginfo('template_url') ;?>/js/jquery.magnific-popup.min.js"></script>
+    <script src="<?php bloginfo('template_url') ;?>/js/main.js"></script>
+    <?php wp_footer(); ?>
+</body>
---
+++ header.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Stroll - Portfolio</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="Stroll Portfolio Template">
+    <meta name="keywords" content="personal, portfolio">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Favicon -->
+    <link href="favicon.ico" rel="shortcut icon" type="image/x-icon" />
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="<?php bloginfo('template_url') ;?>/css/bootstrap.css" />
+    <link rel="stylesheet" href="<?php bloginfo('template_url') ;?>/css/reset.css" />
+    <link rel="stylesheet" href="<?php bloginfo('template_url') ;?>/css/style.css" />
+    <link rel="stylesheet" href="<?php bloginfo('template_url') ;?>/css/animate.css" />
+    <link rel="stylesheet" href="<?php bloginfo('template_url') ;?>/css/magnific-popup.css" />
+
+    <!-- Google Web fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+
+    <!-- Font icons -->
+    <link rel="stylesheet" href="icon-fonts/font-awesome-4.5.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="icon-fonts/essential-regular-fonts/essential-icons.css" />
+
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <?php wp_head(); ?>
+</head>
